e672fe73ab91f96f6918f07f15f26745e94ff07f
LEELA D <88545963+leela2001@users.noreply.github.com>
2024-09-02 15:12:12 +0530

    Update Missing _in_Array.java

--- coding pro/Missing _in_Array.java
+++ coding pro/Missing _in_Array.java
@@ -1,1 +1,27 @@
-bbbk
+//https://youtu.be/iCrQG_kbuP0?si=Sjv7XoLUzHG6wZZS
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// User function Template for Java
+class Solution {
+
+    // Note that the size of the array is n-1
+    int missingNumber(int n, int arr[]) {
+
+        // Your Code Here
+        int allXOR = 0;
+        for(int i = 0; i <= n; i++)
+        {
+            allXOR = allXOR ^ i;
+        }
+        for(int num: arr)
+        {
+            allXOR = allXOR^num;
+        }
+        
+        return allXOR;
+    }
+}
